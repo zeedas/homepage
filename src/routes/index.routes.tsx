6b12cac6da9cabbd90d255fe87973b6ff3eb6f80
Olaolu Johnson <johnsonolaolu@gmail.com>
2022-08-14 13:34:53 +0100

    updated routes

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import PrivateRoutes from './private.routes'
+import PublicRoutes from './public.routes'
 
 const IndexRoutes = () => {
   return (
-    <div>IndexRoutes</div>
+    <Routes>
+      <Route element={<PublicRoutes/>}></Route>
+      <Route element={<PrivateRoutes/>}></Route>
+    </Routes>
   )
 }
 
